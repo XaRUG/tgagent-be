a2052c7601b25071d91fd35542dbe318aea05df8
Bob <bob@Bobui-MacBookPro.local>
2023-02-06 16:23:52 +0900

    update code of rpc setting.

--- src/rpc.rs
+++ src/rpc.rs
@@ -1,15 +1,27 @@
 use anyhow::{Context, Result};
 use jsonrpc_http_server::*;
 use jsonrpc_http_server::jsonrpc_core::*;
+use serde_json::json;
 
 pub fn new_server(address: &String, port: u16, allow_cors: bool) -> Result<Server> {
     let mut io = IoHandler::default();
-	io.add_method("ping", |_: Params| {
+    io.add_method("ping", |_: Params| {
 	    Ok(Value::String("pong".into()))
-	});
+    });
+    io.add_method("tic", |_: Params| {
+	    Ok(Value::String("tok".into()))
+    });
+    io.add_method("version", |params: Params| {
+        let message = json!({
+            "mgessage": "OK",
+            "params": params
+        });
+	    Ok(message)
+    });
 
     let server = ServerBuilder::new(io)
         .cors(DomainsValidation::AllowOnly(vec![AccessControlAllowOrigin::Null]))
+        .rest_api(RestApi::Secure)
         .start_http(&format!("{address}:{port}").parse()?)
         .context("Failed to launch rpc server")?;
 
